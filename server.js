8bb200ca94c0b83740418698795c30ab80ca1a3e
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2017-04-17 16:29:26 +0200

    Add body parser

--- server.js
+++ server.js
@@ -2,44 +2,48 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 var JsonDB = require('node-json-db');
+var bodyParser = require('body-parser');
 
-app.get('/listUsers', function(req, res) {
-    fs.readFile(__dirname + "/" + "users.json", 'utf8', function(err, data) {
-        console.log(data);
-        res.end(data);
-    });
-});
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false };
 
-var server = app.listen(8081, function() {
-    var host = server.address().address
-    var port = server.address().port
-
-    console.log("Example app listening at http://%s:%s", host, port);
-});
-
-
-app.get('/addUsers', function(req, res) {
-    fs.readFile('users.json', 'utf8', function(err, data) {
-        if (err) {
-            return console.error(err);
-        }
-        console.log("Asynchronous read: " + data.toString());
-        var obj = JSON.parse(data);
-        obj.user10 = ({
-            "name": "mohit4",
-            "password": "password6",
-            "profession": "teacher",
-            "id": 8
+        app.get('/listUsers', function(req, res) {
+            fs.readFile(__dirname + "/" + "users.json", 'utf8', function(err, data) {
+                console.log(data);
+                res.end(data);
+            });
         });
-        var json = JSON.stringify(obj);
-        fs.writeFile('users.json', json, 'utf8');
-    });
-});
+
+        var server = app.listen(8081, function() {
+            var host = server.address().address
+            var port = server.address().port
+
+            console.log("Example app listening at http://%s:%s", host, port);
+        });
 
 
-app.get('/addTable', function(req, res) {
-    var db = new JsonDB("table.json", true, false);
-    db.push("table.json/table[]", { id: 86, squere: 5856 }, true);
+        app.get('/addUsers', function(req, res) {
+            fs.readFile('users.json', 'utf8', function(err, data) {
+                if (err) {
+                    return console.error(err);
+                }
+                console.log("Asynchronous read: " + data.toString());
+                var obj = JSON.parse(data);
+                obj.user10 = ({
+                    "name": "mohit4",
+                    "password": "password6",
+                    "profession": "teacher",
+                    "id": 8
+                });
+                var json = JSON.stringify(obj);
+                fs.writeFile('users.json', json, 'utf8');
+            });
+        });
 
-    console.log(db);
-});
+
+        app.post('/addTable', function(req, res) {
+            var db = new JsonDB("table.json", true, false);
+            db.push("table.json/table[]", { id: 86, squere: 5856 }, true);
+
+            console.log(db);
+        });
