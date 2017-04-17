55ea2652226bc8d82d0b13472144070324083884
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2017-04-17 15:52:24 +0200

    add new table json doesn 't work

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
-
+var es6 = require('./ES6.js');
 
 app.get('/listUsers', function(req, res) {
     fs.readFile(__dirname + "/" + "users.json", 'utf8', function(err, data) {
@@ -18,22 +18,37 @@ var server = app.listen(8081, function() {
 });
 
 
-
-
-
-
-fs.readFile('users.json', 'utf8', function(err, data) {
-    if (err) {
-        return console.error(err);
-    }
-    console.log("Asynchronous read: " + data.toString());
-    var obj = JSON.parse(data);
-    obj.user10 = ({
-        "name": "mohit4",
-        "password": "password6",
-        "profession": "teacher",
-        "id": 8
+app.get('/addUsers', function(req, res) {
+    fs.readFile('users.json', 'utf8', function(err, data) {
+        if (err) {
+            return console.error(err);
+        }
+        console.log("Asynchronous read: " + data.toString());
+        var obj = JSON.parse(data);
+        obj.user10 = ({
+            "name": "mohit4",
+            "password": "password6",
+            "profession": "teacher",
+            "id": 8
+        });
+        var json = JSON.stringify(obj);
+        fs.writeFile('users.json', json, 'utf8');
     });
-    var json = JSON.stringify(obj);
-    fs.writeFile('users.json', json, 'utf8');
+});
+
+app.get('/addtable', function(req, res) {
+    fs.readFile('table.json', 'utf8', function(err, data) {
+        if (err) {
+            return console.error(err);
+        }
+        console.log("Asynchronous read: " + data.toString());
+        var obj = JSON.parse(data);
+
+        //try get last id from obj.
+
+
+        obj.table.push({ id: 11, square: 20 });
+        var json = JSON.stringify(obj);
+        fs.writeFile('table.json', json, 'utf8');
+    });
 });
